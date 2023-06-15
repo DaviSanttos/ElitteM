0f0ea88ae0fd064f8271f62f89a617ba7928aee3
DaviSanttos <davicelso.santos@hotmail.com>
2023-06-14 21:19:51 -0300

    fist-commit

--- elitteMoveis-planejamento/Banco de dados/banco2.sql
+++ elitteMoveis-planejamento/Banco de dados/banco2.sql
@@ -85,29 +85,42 @@ CREATE TABLE logg(
     FOREIGN KEY (fk_usuario) REFERENCES usuario(id_usuario)
 );
 
-
--- delimiter $
--- create trigger trg_cadped
--- after delete on pedido
--- for each row
--- begin
--- 	insert into logg (qtd, preco, fk_pedido,fk_material,fk_projeto,fk_usuario)
---     values (old.qtd,old.preco,old.id_pedido,old.fk_material,old.fk_projeto,old.fk_usuario);
--- end$
--- delimiter ;
-
--- delimiter $
--- create trigger trg_insped
--- after update on pedido
--- for each row
--- begin
--- 	insert into logg (qtd, preco, fk_pedido,fk_material,fk_projeto,fk_usuario)
---     values (old.qtd - new.qtd,new.preco,new.id_pedido,new.fk_material,new.fk_projeto,new.fk_usuario);
--- end$
--- delimiter ;
-
 insert into usuario(nome_usuario, senha, nivel) values("user", "$2y$10$.wADUyVVO12LTvy789/GoObncLjcH8LJFs1Kb6KyLwMjuIPYxkMJK", 0);
 insert into usuario(nome_usuario, senha, nivel) values("adm", "$2y$10$.wADUyVVO12LTvy789/GoObncLjcH8LJFs1Kb6KyLwMjuIPYxkMJK", 1);
 
--- DROP TRIGGER trg_cadped;
--- DROP TRIGGER trg_insped;
+
+insert into categorias(id_categoria, nome_categoria) values ("0", "Acabamentos");
+insert into categorias(id_categoria, nome_categoria) values ("0", "Acessórios");
+insert into categorias(id_categoria, nome_categoria) values ("0", "Insumos");
+insert into categorias(id_categoria, nome_categoria) values ("0", "Chapas");
+
+insert into marcas(id_marca, nome_marca) values ("0", "Rehau");
+insert into marcas(id_marca, nome_marca) values ("0", "Proadec");
+insert into marcas(id_marca, nome_marca) values ("0", "Perfilare");
+insert into marcas(id_marca, nome_marca) values ("0", "Hafele");
+
+insert into fornecedores(id_fornecedor, nome_fornecedor) values ("0", "Gmad");
+insert into fornecedores(id_fornecedor, nome_fornecedor) values ("0", "Madeiranit");
+insert into fornecedores(id_fornecedor, nome_fornecedor) values ("0", "Rometal");
+insert into fornecedores(id_fornecedor, nome_fornecedor) values ("0", "JC Tintas");
+
+
+insert into materiais(id_material, nome_material,preco,fk_fornecedor,fk_categoria,fk_marca) values ("0", "FITA BORDA CARVALHO ÉVORA 35MM 20M", 68.05, 1,1,1);
+insert into materiais(id_material, nome_material,preco,fk_fornecedor,fk_categoria,fk_marca) values ("0", "FITA BORDA MINT 22MM 20M
+", 45.24, 1,1,1);
+insert into materiais(id_material, nome_material,preco,fk_fornecedor,fk_categoria,fk_marca) values ("0", "FITA BORDA FREIJÓ PURO - ESSENCIAL WOOD 22MM 20M", 48.02 , 1,1,1);
+insert into materiais(id_material, nome_material,preco,fk_fornecedor,fk_categoria,fk_marca) values ("0", "FITA BORDA TEKA BIANCO RAVENA 65MM", 96.00 , 1,1,1);
+
+
+
+insert into materiais(id_material, nome_material,preco,fk_fornecedor,fk_categoria,fk_marca) values ("0", 'PARAFUSO FRANC 5/16" X 6 
+', 1.60, 3,3,4);
+insert into materiais(id_material, nome_material,preco,fk_fornecedor,fk_categoria,fk_marca) values ("0", "CORREDIÇA TEL. 550MM
+", 24.50, 3,3,4);
+
+
+
+insert into materiais(id_material, nome_material,preco,fk_fornecedor,fk_categoria,fk_marca) values ("0", "CHAPA MDF CANOVAS 6MM
+", 184.00, 2,4,2);
+insert into materiais(id_material, nome_material,preco,fk_fornecedor,fk_categoria,fk_marca) values ("0", "CHAPA MDF ATENNA 15MM", 386.22,2,4,2);
+
